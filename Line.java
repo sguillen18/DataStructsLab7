fe4418e65850b1795bcd1e90855b891a70283dec
sguillen18 <36303985+sguillen18@users.noreply.github.com>
2018-11-15 15:46:07 -0500

    Add files via upload

---
+++ Line.java
@@ -0,0 +1,20 @@
+package lab7;
+
+public class Line {
+	
+	private class Point2d{
+		
+		
+		
+	}
+	
+	//in professor's prompt, y2 is actually just y
+	public Line(double x1, double y1, double x2, double y2) {
+		
+	}
+	
+	public Line(Point2d start, Point2d end) {
+		
+	}
+
+}
