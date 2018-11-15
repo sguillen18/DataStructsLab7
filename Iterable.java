fc843e84c992123b01ea437730fba3b1a35d8cb5
trzsroo <33383490+trzsroo@users.noreply.github.com>
2018-11-15 15:41:20 -0500

    Add files via upload

--- Iterable.java
+++ Iterable.java
@@ -1,5 +1,31 @@
 package Lab7;
 
 public interface Iterable <T> {
+	/** Returns true if the iteration has more elements.
+	  * (In other words, returns true if next would
+	  * return an element rather than throwing an exception.)
+	  * @return true if the iteration has more elements,  false otherwise
+	  */
+	 public boolean hasNext();
+
+	 /** Returns the next element in the iteration.
+	  * @return the next element in the iteration
+	  * @throws NoSuchElementException if the iteration has no more elements
+	  */
+	  public T next();
+
+	 /** Removes from the underlying collection the last element returned
+	  * by this iterator (optional operation). This method can be called
+	  * only once per call to next.  The behavior of an iterator is 
+	  * unspecified if the underlying collection is modified while the
+	  * iteration is in progress in any way other than by calling this method.
+	  * @throws UnsupportedOperationException if the remove
+	  * operation is not supported by this iterator
+	  * @throws IllegalStateException if the next method has not yet been 
+	  * called, or the remove method has already been called after the last
+	  * call to the next method
+	  */ 
+	  public void remove();
+
 
 }
